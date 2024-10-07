e5eb55c1e986f603972b2291ced5f77f7a4b63e9
Rychardson <rychardson.souza.114@ufrn.edu.br>
2024-10-06 21:20:13 -0300

    add lab 1 - eng dados

---
+++ clienteTCP.py
@@ -0,0 +1,28 @@
+# PROF CARLOS M D VIEGAS (C)
+# carlos.viegas '@' ufrn.br
+#
+# ----------------------------------------------- 
+#
+# SCRIPT: CLIENTE TCP (python3)
+#
+# COMO EXECUTAR?
+# ?> python clienteTCP.py <endereco-ip-do-servidor>
+#
+
+# importacao das bibliotecas
+from socket import *
+import sys
+import time
+
+# definicao das variaveis
+serverName = str(sys.argv[1])
+serverPort = 30000 # porta a se conectar
+clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
+clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
+
+sentence = 'container id (hostname) ' + gethostname() + ' / ip ' + gethostbyname(gethostname())
+print ('> enviando para o servidor -> %s' % sentence)
+clientSocket.send(sentence.encode('utf-8')) # envia o texto para o servidor
+time.sleep(1)
+clientSocket.close() # encerramento o socket do cliente
+time.sleep(2)
---
+++ servidorTCP.py
@@ -0,0 +1,28 @@
+# PROF CARLOS M D VIEGAS (C)
+# carlos.viegas '@' ufrn.br
+#
+# ----------------------------------------------- 
+#
+# SCRIPT: SERVIDOR TCP (python3)
+#
+# COMO EXECUTAR?
+# ?> python servidorTCP.py
+#
+
+# importacao das bibliotecas
+from socket import * # sockets
+
+# definicao das variaveis
+serverPort = 30000 # porta a servir
+serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
+serverSocket.bind(('',serverPort)) # bind do ip do servidor com a porta
+serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
+serverIP=gethostbyname(gethostname())
+print ('> servidor iniciado em %s:%d ...' % (serverIP,serverPort))
+while 1:
+  connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
+  sentence = connectionSocket.recv(1024) # recebe dados do cliente
+  sentence = sentence.decode('utf-8') # codifica em utf-8
+  print ('> mensagem recebida de %s -> %s' % (addr, sentence))
+  connectionSocket.close() # encerra o socket com o cliente
+serverSocket.close() # encerra o socket do servidor
